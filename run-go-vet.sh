7b65ad2faa16486623820eacc6b9dca63b1571b5
Francisco Daines <fdaines@gmail.com>
2021-05-24 16:39:49 -0400

    Update run-go-vet.sh

--- run-go-vet.sh
+++ run-go-vet.sh
@@ -15,19 +15,9 @@ for item in "$@";do
 done
   
 pkg=$(go list)
-echo "1-------------------"
-echo $pkg
-echo "2-------------------"
-echo $@
-echo "3-------------------"
-echo ${params[@]}
-echo "4-------------------"
-echo ${files[@]}
-echo "5-------------------"
-for dir in $(echo $@|xargs -n1 dirname|sort -u); do
+for dir in $(echo ${files[@]}|xargs -n1 dirname|sort -u); do
   echo "x1-------------------"
   echo $pkg/$dir
-  echo "x2-------------------"
   go vet $pkg/$dir
   echo "x3-------------------"
 done
