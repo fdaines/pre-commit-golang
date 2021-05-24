53021e841fbef5514a056f9697472b74d7d9ced1
Francisco Daines <fdaines@gmail.com>
2021-05-24 16:04:43 -0400

    Update run-go-vet.sh

--- run-go-vet.sh
+++ run-go-vet.sh
@@ -1,10 +1,19 @@
 #!/usr/bin/env bash
 set -e
 pkg=$(go list)
+echo "1-------------------"
 echo $pkg
+echo "2-------------------"
 echo $@
+echo "3-------------------"
+echo $(echo $@|xargs -n1 dirname)
+echo "4-------------------"
 echo $(echo $@|xargs -n1 dirname|sort -u)
+echo "5-------------------"
 for dir in $(echo $@|xargs -n1 dirname|sort -u); do
+  echo "x1-------------------"
   echo $pkg/$dir
+  echo "x2-------------------"
   go vet $pkg/$dir
+  echo "x3-------------------"
 done
