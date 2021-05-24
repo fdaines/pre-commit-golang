33b421f8efd09c630b37e707a57c1a0a9d9c0f92
Francisco Daines <fdaines@gmail.com>
2021-05-24 16:38:01 -0400

    Update run-go-vet.sh

--- run-go-vet.sh
+++ run-go-vet.sh
@@ -20,9 +20,9 @@ echo $pkg
 echo "2-------------------"
 echo $@
 echo "3-------------------"
-echo $params[@]
+echo ${params[@]}
 echo "4-------------------"
-echo $files[@]
+echo ${files[@]}
 echo "5-------------------"
 for dir in $(echo $@|xargs -n1 dirname|sort -u); do
   echo "x1-------------------"
