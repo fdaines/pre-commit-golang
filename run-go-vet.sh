4f6f4c817a22cd77d02e05a64f2e2be37ed802a6
Francisco Daines <fdaines@gmail.com>
2021-05-24 16:43:31 -0400

    Update run-go-vet.sh

--- run-go-vet.sh
+++ run-go-vet.sh
@@ -3,21 +3,15 @@ set -e
 files=()
 params=()
 for item in "$@";do
-  echo "Current item: $item"
   if [[ $item == -* ]]
   then
-     echo "is a param"
      params+=($item)  
   else
-     echo "is a file"
      files+=($item)
   fi
 done
   
 pkg=$(go list)
 for dir in $(echo ${files[@]}|xargs -n1 dirname|sort -u); do
-  echo "x1-------------------"
-  echo $pkg/$dir
   go vet ${params[@]} $pkg/$dir
-  echo "x3-------------------"
 done
