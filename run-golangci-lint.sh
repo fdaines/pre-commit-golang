f906c362c94d8c51fe0224ebbd908a2bc4fdb49b
Francisco Daines <fdaines@gmail.com>
2021-05-25 11:41:08 -0400

    Update run-golangci-lint.sh

--- run-golangci-lint.sh
+++ run-golangci-lint.sh
@@ -1,2 +1,6 @@
-#!/usr/bin/env sh
-exec golangci-lint run "$@"
+#!/usr/bin/env bash
+set -e
+for dir in $(echo $@|xargs -n1 dirname|sort -u); do
+  echo "Run over $dir"
+  golangci-lint run $dir
+done
